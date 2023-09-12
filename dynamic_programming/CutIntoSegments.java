13861bb6d178fab516847357bdd6ec709304abe4
Anuj Saxena <anuj.saxena@hungerbox.com>
2023-09-12 12:26:13 +0530

    Rod cutting Problem Tablularization

--- dynamic_programming/CutIntoSegments.java
+++ dynamic_programming/CutIntoSegments.java
@@ -6,7 +6,8 @@ public class CutIntoSegments {
         for(int i = 0; i < n + 1; i++){
             dp[i] = -1;
         }
-        int result = cutSegmentsEval(n,x,y,z,dp);
+        // int result = cutSegmentsEval(n,x,y,z,dp);
+        int result = cutSegmentsTab(n,x,y,z);
         if(result < 0){
             return 0;
         }
@@ -34,9 +35,54 @@ public class CutIntoSegments {
         }
     }
 
+    private static int cutSegmentsTab(int n, int x, int y, int z){
+        if(n < 0){
+            return Integer.MIN_VALUE;
+        }
+        else if(n == 0){
+            return 0;
+        }
+        else {
+            int[] dp = new int[n + 1];
+            for(int i = 0; i < n + 1; i++){
+                if(i == 0){
+                    dp[i] = 0;
+                }
+                else {
+                    dp[i] = -1;
+                }
+            }
+            for(int i = 1; i < n + 1; i++){
+                int ansX = -1;
+                if(i - x >= 0){
+                    ansX = dp[i - x];
+                }
+                int ansY = -1;
+                if(i - y >= 0){
+                    ansY = dp[i - y];
+                }
+                int ansZ = -1;
+                if(i - z >= 0){
+                    ansZ = dp[i - z];
+                }
+                int max = max(ansX, max(ansY, ansZ));
+                if(max != -1){
+                    dp[i] = max + 1;
+                }
+            }
+            if(dp[n] != -1){
+                return dp[n];
+            }
+            else {
+                return 0;
+            }
+        }
+    }
+
     private static int max(int a, int b){
         return a > b ? a : b;
     }
+
     public static void main(String[] args){
         System.out.println(cutSegments(8,3,3,3));
     }
