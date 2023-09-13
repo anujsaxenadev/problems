690934ce83f7362aa6b07c6daa7404052c5d2d87
Anuj Saxena <anuj.saxena@hungerbox.com>
2023-09-13 13:19:11 +0530

    Space Optimization

--- dynamic_programming/CountDerangements.java
+++ dynamic_programming/CountDerangements.java
@@ -52,12 +52,30 @@ public class CountDerangements {
         }
 	}
 
+    public static long countDerangementsTabSpace(int n) {
+        if(n == 0 || n == 1){
+            return 0;
+        }
+        else if(n == 2){
+            return 1;
+        }
+        else {
+            long prev2 = 0, prev1 = 1;
+            for(int i = 3; i <= n; i++){
+                long current = (((i - 1) % MOD) * ((prev2 % MOD) + (prev1 % MOD)) % MOD) % MOD;
+                prev2 = prev1;
+                prev1 = current;
+            }
+            return prev1;
+        }
+	}
+
     public static void main(String[] args){
         int n = 2078;
         long[] dp = new long[n + 1];
         for(int i = 0; i < n + 1; i++){
             dp[i] = -1;
         }
-        System.out.println(countDerangementsTab(n));
+        System.out.println(countDerangementsTabSpace(n));
     }
 }
