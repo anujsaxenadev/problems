feb513da5a88564af3d11b51bc0ee31e75ce63fc
Anuj Saxena <anuj.saxena@hungerbox.com>
2023-09-14 18:02:37 +0530

    Space Optimization

--- dynamic_programming/NinjaAndFence.java
+++ dynamic_programming/NinjaAndFence.java
@@ -3,11 +3,13 @@ package dynamic_programming;
 public class NinjaAndFence {
     private static int MOD = 1000000007;
     private static int numberOfWays(int n, int k) {
-        int[] dp = new int[n + 1];
-        for(int i = 0; i < n + 1; i++){
-            dp[i] = -1;
-        }
-        return findNumberOfWaysMem(n, k, dp);
+        // return findNumberOfWaysRec(n, k);
+        // int[] dp = new int[n + 1];
+        // for(int i = 0; i < n + 1; i++){
+        //     dp[i] = -1;
+        // }
+        // return findNumberOfWaysMem(n, k, dp);
+        return findNumberOfWaysTab(n, k);
     }
 
     private static int add(int a, int b){
@@ -42,6 +44,25 @@ public class NinjaAndFence {
         else{
             dp[n] = add(mul(k - 1, findNumberOfWaysMem(n - 1, k, dp)), mul(k - 1, findNumberOfWaysMem(n - 2, k, dp)));
             return dp[n];
+        }
+    }
+
+    private static int findNumberOfWaysTab(int n, int k){
+        if(n == 1){
+            return k;
+        }
+        else if(n == 2){
+            return mul(k, k);
+        }
+        else{
+            int prev2 = k;
+            int prev1 = mul(k, k);
+            for(int i = 3; i < n + 1; i++){
+                int temp = add(mul(k - 1, prev1), mul(k - 1, prev2));
+                prev2 = prev1;
+                prev1 = temp;
+            }
+            return prev1;
         }
     }
 
