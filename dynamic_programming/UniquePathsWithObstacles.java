5c169d1781588a2a49081769585e189618154eec
anujsaxenadev <anuj.saxena.dev@gmail.com>
2023-10-05 08:14:53 +0530

    Check Unique Paths Memoization Methods

--- dynamic_programming/UniquePathsWithObstacles.java
+++ dynamic_programming/UniquePathsWithObstacles.java
@@ -1,9 +1,13 @@
 package dynamic_programming;
 
+import java.util.HashMap;
+
 public class UniquePathsWithObstacles {
+    static HashMap<String, Integer> valueMap = new HashMap<>();
     public static void main(String[] args) {
-        int[][] input = new int[1][2];
+        int[][] input = new int[2][2];
         input[0] = new int[]{0,0};
+        input[1] = new int[]{0,0};
         System.out.println(uniquePathsWithObstacles(input));
     }
     public static int uniquePathsWithObstacles(int[][] obstacleGrid) {
@@ -11,7 +15,32 @@ public class UniquePathsWithObstacles {
             return 0;
         }
         else {
-            return checkUniquePathsRecursively(obstacleGrid, 0, 0, obstacleGrid[0].length, obstacleGrid.length);
+            return checkUniquePathsMem(obstacleGrid, 0, 0, obstacleGrid[0].length, obstacleGrid.length);
+        }
+    }
+
+    public static int checkUniquePathsMem(int[][] obstacleGrid, int x, int y, int xLength, int yLength){
+        if(x == xLength - 1 && y == yLength - 1 && obstacleGrid[y][x] != 1){
+            return 1;
+        }
+        else if(obstacleGrid[y][x] == 1){
+            return 0;
+        }
+        else if(valueMap.containsKey(x + "," + y)){
+            return valueMap.get(x + "," + y);
+        }
+        else {
+            int bottomResult = 0;
+            if(x + 1 < xLength){
+                bottomResult = checkUniquePathsMem(obstacleGrid, x + 1, y, xLength, yLength);
+                valueMap.put((x + 1) + "," + y, bottomResult);
+            }
+            int rightResult = 0;
+            if(y + 1 < yLength){
+                rightResult = checkUniquePathsMem(obstacleGrid, x, y + 1, xLength, yLength);
+                valueMap.put(x + "," + (y + 1), rightResult);
+            }
+            return rightResult + bottomResult;
         }
     }
 
