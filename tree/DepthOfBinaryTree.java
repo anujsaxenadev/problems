3168e9cb80e87c209cbc5cf1cd541f9e1d5eab5d
anujsaxenadev <anuj.saxena.dev@gmail.com>
2023-10-15 22:34:51 +0530

    Depth of Binary Tree

--- tree/DepthOfBinaryTree.java
+++ tree/DepthOfBinaryTree.java
@@ -1,24 +1,6 @@
 package tree;
 
 public class DepthOfBinaryTree {
-    private int findDepth(TreeNode root, int level){
-        if(root == null){
-            return -1;
-        }
-        else if(root.left == null && root.right == null){
-            return level;
-        }
-        else{
-            int max = 0;
-            if(root.left != null){
-                max = Math.max(findDepth(root.left, level + 1), max);
-            }
-            if(root.right != null){
-                max = Math.max(findDepth(root.right, level + 1), max);
-            }
-            return max;
-        }
-    }
     public int maxDepth(TreeNode root) {
         if(root == null){
             return 0;
